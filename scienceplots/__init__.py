e63048c6f507cec54ca06849032bf86f05e874ea
ChongChong He <chongchong.he@anu.edu.au>
2025-08-09 14:14:25 +1000

    figsize

--- scienceplots/__init__.py
+++ scienceplots/__init__.py
@@ -16,3 +16,5 @@ plt.style.core.update_nested_dict(plt.style.library, stylesheets)
 # Update `plt.style.available`, copy-paste from:
 # https://github.com/matplotlib/matplotlib/blob/a170539a421623bb2967a45a24bb7926e2feb542/lib/matplotlib/style/core.py#L266  # noqa: E501
 plt.style.core.available[:] = sorted(plt.style.library.keys())
+
+figsize = [3.5, 2.625]
